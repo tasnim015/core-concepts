d177a1b883b1b5ff878e62b02e4ca9e8a171b924
tasnim015 <tasnimulhasan78@gmail.com>
2020-08-21 07:45:14 +0600

    react core concepts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +6,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>I am a react person</p>
+       <Counter></Counter>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
       </header>
     </div>
   );
 }
+function Person(){
+  const personStyle = {
+    border: '2px solid red',
+    margin: '3px',
+    padding: '20px'
+  }
+  return (
+  <div style={personStyle}>
+    <h1>Shakib Al Hassan</h1>
+    <h3>hero of the year</h3>
+  </div>
+  );
+}
+function Counter(){
+  const [count,setCount] = useState(10);
+  const handleIncrease = () => setCount(count + 1);
+  return(
+    <div>
+      <h1>COUNT: {count}</h1>
+      <button onClick={handleIncrease}>increase</button>
+      <button onClick={ () => setCount(count - 1)}>decrease</button>
+    </div>
+  )
+}
+
 
 export default App;
